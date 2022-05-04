84e2fc1fcec57da517735714799108898ed28b85
suddar <37994239+suddar@users.noreply.github.com>
2022-05-04 10:16:07 +0700

    Create IStudentService, StudentService Register StudentService

---
+++ Services/IStudentService.cs
@@ -0,0 +1,14 @@
+﻿using StudentExample.Entities;
+
+namespace StudentExample.Services
+{
+    public interface IStudentService
+    {
+        List<Student> GetStudents();
+        bool AddStudent(Student student);
+        Student getStudent(int id);
+        bool UpdateStudent(int id, Student student);
+        bool RemoveStudent(int id);
+
+    }
+}
