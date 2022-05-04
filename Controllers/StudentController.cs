bd8e1474db4cea63903f641bdd406d8c81856479
suddar <37994239+suddar@users.noreply.github.com>
2022-05-04 09:32:49 +0700

    Create  StudentController Api

---
+++ Controllers/StudentController.cs
@@ -0,0 +1,96 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StudentExample.Entities;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace StudentExample.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentController : ControllerBase
+    {
+        List<Student> studentlist = new List<Student>();
+
+        public StudentController()
+        {
+            Student student = new Student();
+            student.Name = "Mai Thi Hoa";
+            student.Address = "Ha noi";
+
+            studentlist.Add(student);
+            studentlist.Add(new());
+        }
+
+
+        [HttpGet]
+        public ActionResult Get()
+        {
+            try
+            {
+                return Ok(studentlist);
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError,
+                    "Error retrieving data from the database");
+            }
+        }
+
+        // get student by id
+        [HttpGet("{id}")]
+        public ActionResult<Student> GetEmployee(int id)
+        {
+            try
+            {
+                var result = studentlist[id];
+
+                if (result == null) return NotFound();
+
+                return result;
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError,
+                    "Error retrieving data from the database");
+            }
+        }
+
+        // POST a student
+        [HttpPost]
+        public IActionResult Post([FromBody] Student value)
+        {
+            studentlist.Add(value);
+
+            return Ok(value);
+        }
+
+        // PUT api/<StudentController>/5
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, [FromBody] Student value)
+        {
+            studentlist[id] = value;
+            return Ok(studentlist[id]);
+        }
+
+        // DELETE api/<StudentController>/5
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            try
+            {
+                var studentToRemove = studentlist[id];
+
+                if (studentToRemove == null)
+                {
+                    return NotFound($"Student with Id = {id} not found");
+                }
+
+                return Ok();
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "Error deleting data");
+            }
+        }
+    }
+}
---
+++ Entities/Student.cs
@@ -0,0 +1,13 @@
+﻿namespace StudentExample.Entities
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime Date { get; set; }
+        public DateTime CreateTime { get; set; }
+        public string Address { get; set; }
+        public string Class { get; set; }
+        public int Count { get; set; } = 0;
+    }
+}
